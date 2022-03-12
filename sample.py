0c1feb3e51631be6eaed5d93e98d9b61dd98cf71
Hughes de Saxcé <hugues.de.saxce@protonmail.com>
2022-03-12 08:56:23 -0500

    [1] Add a class Sample which contains the match-up's features & label and the teams' IDs

---
+++ sample.py
@@ -0,0 +1,11 @@
+class Sample:
+    """
+    A sample wraps the features and the label of a specific match-up team_1 vs. team_2.
+
+    When creating Sample objects for prediction purposes, the label value does not matter.
+    """
+    def __init__(self, team_1_id: int, team_2_id, features: [float], label: int):
+        self.team_1_id: int = team_1_id
+        self.team_2_id: int = team_2_id
+        self.features: [float] = features
+        self.label: int = label
