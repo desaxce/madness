0d383aee86a78c5404af64460fa719968d3e3d17
Hughes de Saxcé <hugues.de.saxce@protonmail.com>
2022-03-13 10:34:38 -0400

    Use feature classes to build samples

--- feature.py
+++ feature.py
@@ -1,16 +1,19 @@
 class AbsoluteFeature:
     """
-    Represents a feature intrinsic to a team and not related to its opponent.
+    Represents a feature intrinsic to each team and not related to its opponent.
 
     E.g. team ID, reg. season avg pts allowed, reg. season game records, etc.
     """
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, team_1_value: float, team_2_value: float):
+        self.value_1: float = team_1_value
+        self.value_2: float = team_2_value
 
 
 class RelativeFeature:
     """
     Represents a feature tied to a specific match-up team_1 vs. team_2.
+    Stores both "visions" of the match-up.
     """
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, team_1_vs_team_2_value: float, team_2_vs_team_1_value: float):
+        self.value_1 = team_1_vs_team_2_value
+        self.value_2 = team_2_vs_team_1_value
